95ea147ab0a5466a2cf61644b006216d5ed1a947
mat.boen <mat.boen@gmail.com>
2016-03-24 16:41:27 +0100

    login view en functie

--- controllers/SqlFuncties.php
+++ controllers/SqlFuncties.php
@@ -34,4 +34,33 @@
 		// }	
 		// return $array;
 	// }
+	
+	function Login($mail,$paswoord)
+	{
+		global $link;
+		
+		$mail = mysqli_real_escape_string($link, $mail);	
+		$password = mysqli_real_escape_string($link, $paswoord);
+		$password = hash("sha512", $password);
+		
+		$result = mysqli_query($link, "SELECT * FROM user WHERE mail='".$mail."'");
+		
+		$row = mysqli_fetch_assoc($result);
+		
+		if($mail == $row['mail'] && $password == $row['paswoord'])
+		{
+			unset($_SESSION['user']);
+			$_SESSION['user'] = 1000;
+			$_SESSION['ID']=$row['userID'];
+			header('Location: ../inTeStellen.php');
+			exit();
+		}
+		else
+		{
+			unset($_SESSION['fout']);
+			$_SESSION['fout'] = 'Login is fout!';
+			exit();
+		}
+		mysqli_close($link);
+	}
 ?>
---
+++ views/login.php
@@ -0,0 +1,5 @@
+<form method="post" action="../controllers/loginVerwerk.php">
+	<input type="text" name="mail" placeholder="naam@mail.com" required></input><br>
+	<input type="password" name="paswoord" placeholder="wachtwoord" required></input><br>
+	<input type="submit" value="Log in"></input><br>
+</form>
