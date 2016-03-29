98b471505d3060c49ddc04ec33a3b079ac6700bf
samme robbroeckx <samme robbroeckx>
2016-03-29 19:27:18 +0200

    slideshow

--- views/home.php
+++ views/home.php
@@ -1,3 +1,32 @@
 <?php
 	echo "home";
 ?>
+
+<div id="myCarousel" class="carousel slide" data-ride="carousel">
+	<!-- Indicators -->
+	<ol class="carousel-indicators">
+		<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+		<li data-target="#myCarousel" data-slide-to="1"></li>
+		<li data-target="#myCarousel" data-slide-to="2"></li>
+		<li data-target="#myCarousel" data-slide-to="3"></li>
+	</ol>
+
+	<!-- Wrapper for slides -->
+	<div class="carousel-inner" role="listbox">
+		<div class="item active">
+			<img src="img/foto1.jpg" alt="Foto" width="auto" height="100vh">
+		</div>
+
+		<div class="item">
+			<img src="img/foto2.jpg" alt="Foto" width="auto" height="100vh">
+		</div>
+
+		<div class="item">
+			<img src="img/foto3.jpg" alt="Foto" width="auto" height="100vh">
+		</div>
+
+		<div class="item">
+			<img src="img/foto4.jpg" alt="Foto" width="auto" height="100vh">
+		</div>
+	</div>
+</div>
