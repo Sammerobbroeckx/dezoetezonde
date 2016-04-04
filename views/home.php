5239237c5238763f6a9e7030e69d02f50027a409
samme robbroeckx <samme robbroeckx>
2016-04-04 18:55:45 +0200

    image cover

--- views/home.php
+++ views/home.php
@@ -1,33 +1,31 @@
 
-<div class="row">
-                <div class="col-lg-12" id="fotogallerij">
-<div id="myCarousel" class="carousel slide" data-ride="carousel">
-	<!-- Indicators -->
-	<ol class="carousel-indicators">
-		<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-		<li data-target="#myCarousel" data-slide-to="1"></li>
-		<li data-target="#myCarousel" data-slide-to="2"></li>
-		<li data-target="#myCarousel" data-slide-to="3"></li>
-	</ol>
+<div id="fotogallerij">
+    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+        <!-- Indicators -->
+        <ol class="carousel-indicators">
+            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+            <li data-target="#myCarousel" data-slide-to="1"></li>
+            <li data-target="#myCarousel" data-slide-to="2"></li>
+            <li data-target="#myCarousel" data-slide-to="3"></li>
+        </ol>
 
-	<!-- Wrapper for slides -->
-	<div class="carousel-inner" role="listbox">
-		<div class="item active">
-			<img src="img/foto1.jpg" alt="Foto" width="100vh" height="100vh">
-		</div>
+        <!-- Wrapper for slides -->
+        <div class="carousel-inner" role="listbox">
+            <div class="item active">
+                <img id="foto1" alt="Foto" width="100vh" height="100vh">
+            </div>
 
-		<div class="item">
-			<img src="img/foto2.jpg" alt="Foto" width="100vh" height="100vh">
-		</div>
+            <div class="item">
+                <img id="foto2" alt="Foto" width="100vh" height="100vh">
+            </div>
 
-		<div class="item">
-			<img src="img/foto3.jpg" alt="Foto" width="100vh" height="100vh">
-		</div>
+            <div class="item">
+                <img id="foto3" alt="Foto" width="100vh" height="100vh">
+            </div>
 
-		<div class="item">
-			<img src="img/foto4.jpg" alt="Foto" width="100vh" height="100vh">
-		</div>
-	</div>
-</div>
-</div>
+            <div class="item">
+                <img id="foto4" alt="Foto" width="100vh" height="100vh">
+            </div>
+        </div>
+    </div>
 </div>
