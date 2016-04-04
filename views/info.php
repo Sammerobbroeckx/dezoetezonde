321d6f2fff52df3a38f6f8b994ed5c9d569a6b9b
Jo Maes <jomaes@Jos-MacBook-Air.home>
2016-04-04 23:06:14 +0100

    nog eens pushen

--- views/info.php
+++ views/info.php
@@ -1,2 +1,38 @@
-<h1 class='PaginaTitel'>Informatie</h1>
-<h2 class='OpeningsurenText'>Openingsuren</h2>
+<h1 class='PaginaTitel'>Openingsuren</h1>
+
+<table id='Openingsuren'>
+  <tr>
+    <td>Maandag</td>
+    <td>Gesloten</td>
+  </tr>
+  <tr>
+    <td>Dinsdag</td>
+    <td>08.00 tot 18.00</td> 
+  </tr>
+  <tr>
+    <td>Woensdag</td>
+    <td>08.00 tot 18.00</td> 
+  </tr>
+  <tr>
+    <td>Donderdag</td>
+    <td>08.00 tot 17.00</td> 
+  </tr>
+  <tr>
+    <td>Vrijdag</td>
+    <td>08.00 tot 18.00</td> 
+  </tr>
+  <tr>
+    <td>Zaterdag</td>
+    <td>08.00 tot 18.00</td> 
+  </tr>
+  <tr>
+    <td>Zondag</td>
+    <td >Gesloten</td> 
+  </tr>
+  
+</table>
+<div id='Facebook'>
+<div  class="fb-like" data-href="http://www.dezoetezonde.info/index.html" data-layout="button" 
+data-action="like" data-show-faces="false" data-share="false">
+</div>
+</div>
