3a8567af9159672d2586855781616d585a98471a
Conner Maes <conner.maes@hotmail.com>
2016-05-03 21:50:51 +0200

    -g

--- views/footer.php
+++ views/footer.php
@@ -21,9 +21,7 @@
             </div>
         </div>
             
-         <div id='footer'>
-            <p>© 2016 - De Zoete Zonde |  Website door Conner Maes en Samme Robbroeckx </p>   
-        </div>
+         
             
         <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
         <script type='text/javascript' src="http://ajax.googleapis.com/ajax/libs/angularjs/1.0.4/angular.js"></script>
--- views/head.php
+++ views/head.php
@@ -6,6 +6,7 @@
 <!DOCTYPE html>
 <html>
 	<head>
+		
 		<title>De Zoete Zonde</title>
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<meta charset="utf-8">
